7447629a65475c91e4ee24106c79960560cc2eca
Ari <ari@fullstack.io>
2017-04-05 21:46:54 -0700

    Prepared branch day-8

---
+++ src/Proptypes1.js
@@ -0,0 +1,8 @@
+class Clock extends React.Component {
+  // ...
+}
+
+Clock.propTypes = {
+  // key is the name of the prop and
+  // value is the PropType
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import 'font-awesome/css/font-awesome.css';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+export const load = () => {
+}
+
+try {
+  //load()
+} catch(e) {}
