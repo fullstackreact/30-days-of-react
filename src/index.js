d79522a8fe2b22826d6be994c7b3618b45143dd7
Ari <ari@fullstack.io>
2017-04-05 21:46:54 -0700

    Prepared branch day-9

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import 'font-awesome/css/font-awesome.css';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import Header from './components/Timeline/Header';
+
+export const load = () => {
+  ReactDOM.render(<Header title="Orange header" />, 
+    document.getElementById('demo1'))
+
+  const style = { color: 'blue' }
+  ReactDOM.render(
+    <div style={style}>
+      This text will have the color blue
+    </div>, 
+    document.getElementById('blueTextDemo'))
+
+  ReactDOM.render(<Header title="Orange header" />, 
+    document.getElementById('demo2'))
+
+  ReactDOM.render(<Header title="Orange header" />, 
+    document.getElementById('demo3'))
+}
+
+try {
+  //load()
+} catch(e) {}
