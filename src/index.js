78c4b3f125224d7e70774f30e7c517e349b3ad11
Ari <ari@fullstack.io>
2017-04-05 21:46:51 -0700

    Prepared branch day-1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+export const load = () => {
+}
