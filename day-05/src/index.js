72b6447da9268faac2623c586ab3f888e768e8bf
Polycarp Masika <masikapolycarp@gmail.com>
2019-11-29 16:02:05 +0300

    (day 5) show all runnable demo code in the lesson post

--- day-05/src/index.js
+++ day-05/src/index.js
@@ -18,7 +18,7 @@ const moment1 = {
     name: 'Nate',
     avatar: 'http://www.croop.cl/UI/twitter/images/doug.jpg'
   },
-  comments: [{from: 'Ari', text: 'Me too!'}]
+  comments: [{ from: 'Ari', text: 'Me too!' }]
 };
 const activities = [
   {
@@ -29,7 +29,7 @@ const activities = [
       name: 'Nate',
       avatar: 'http://www.croop.cl/UI/twitter/images/doug.jpg'
     },
-    comments: [{from: 'Ari', text: 'Me too!'}]
+    comments: [{ from: 'Ari', text: 'Me too!' }]
   },
   {
     timestamp: new Date().getTime(),
@@ -39,13 +39,13 @@ const activities = [
       name: 'Ari',
       avatar: 'http://www.croop.cl/UI/twitter/images/doug.jpg'
     },
-    comments: [{from: 'Nate', text: 'I am so jealous'}]
+    comments: [{ from: 'Nate', text: 'I am so jealous' }]
   }
 ];
 export const load = () => {
   ReactDOM.render(<Timeline />, document.getElementById('demo1'));
 
-  /*ReactDOM.render(
+  ReactDOM.render(
     <Header />,
     document.getElementById('headerDemo1'));
 
@@ -60,25 +60,25 @@ export const load = () => {
       <Header title='Settings' />
       <Header title='Chat' />
     </div>,
-    document.getElementById('demo2'));
+    document.getElementById('headersDemo'));
 
   ReactDOM.render(
     <div className='demo'>
       <Content activity={moment1} />
     </div>,
-    document.getElementById('demo3'));
+    document.getElementById('contentDemo'));
 
   ReactDOM.render(
     <div className='demo'>
       <ContentArr activities={activities} />
     </div>,
-    document.getElementById('demo4'));
+    document.getElementById('contentDemo2'));
 
   ReactDOM.render(
     <div className='demo'>
       <ContentFinal activities={activities} />
     </div>,
-    document.getElementById('demo5'));*/
+    document.getElementById('contentDemo3'));
 };
 
 try {
