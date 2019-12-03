3487246b051b9a6a29a5b855781912e029d50ca2
Polycarp Masika <masikapolycarp@gmail.com>
2019-12-03 17:05:29 +0300

    (day 9) remove CRA related code

--- code/09/header1.js
+++ code/09/header1.js
@@ -1,28 +1,24 @@
 class Header extends React.Component {
   render() {
-    // Classes to add to the <input /> element
-    let searchInputClasses = ["searchInput"];
-
-    // Update the class array if the state is visible
-    if (this.state.searchVisible) {
-      searchInputClasses.push("active");
-    }
-
     return (
-      <div className="header">
-        <div className="fa fa-more"></div>
+      <div
+        className="header"
+        style={{
+          backgroundColor: "rgba(251, 202, 43, 1)"
+        }}
+      >
+        <div className="menuIcon">
+          <div className="dashTop"></div>
+          <div className="dashBottom"></div>
+          <div className="circle"></div>
+        </div>
 
-        <span className="title">
-          {this.props.title}
-        </span>
+        <span className="title">{this.props.title}</span>
 
-        <input
-          type="text"
-          className={searchInputClasses.join(' ')}
-          placeholder="Search ..." />
+        <input type="text" className="searchInput" placeholder="Search ..." />
 
         <div className="fa fa-search searchIcon"></div>
       </div>
-    )
+    );
   }
 }
