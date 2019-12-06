197559ffb7825c27641cfe15df73ec25790d3d6b
Polycarp Masika <masikapolycarp@gmail.com>
2019-12-06 11:44:02 +0300

    (day-11) remove CRA related code

--- day-11/src/components/Timer/Ampm.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Ampm = ({hours}) => (<span>{hours >= 12 ? 'pm' : 'am'}</span>)
-
-export default Ampm
--- day-11/src/components/Timer/Separator.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Separator = ({separator}) => (<span>{separator || ':'}</span>)
-
-export default Separator
