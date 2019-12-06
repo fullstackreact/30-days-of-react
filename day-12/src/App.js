c978e781d209f464dbec3ad0e99f60b2b81bc40e
Polycarp Masika <masikapolycarp@gmail.com>
2019-12-06 14:56:44 +0300

    day 12 CRA fresh install

--- day-12/src/App.js
+++ day-12/src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Header from "./components/Header";
+import Content from "./components/Content";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
---
+++ day-12/src/components/Content.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Content extends Component {
+  render() {
+    return <p className="App-intro">Content goes here</p>;
+  }
+}
+
+export default Content;
