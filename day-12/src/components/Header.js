a258f5616aedc7ab020d9a046440d7b87b589734
Polycarp  Masika <masikapolycarp@gmail.com>
2019-12-06 15:14:37 +0300

    day 12 CRA fresh install (#67)

---
+++ day-12/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div id="header">
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- day-12/src/index.js
+++ day-12/src/index.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-export const load = () => {
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-try {
-  //load()
-} catch(e) {}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
