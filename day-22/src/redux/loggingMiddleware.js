49fd6996b8ed52c388675d9db15db553c29cc980
Polycarp Masika <masikapolycarp@gmail.com>
2019-12-12 14:52:06 +0300

    no code was written on day 22

--- day-22/src/redux/loggingMiddleware.js
+++
@@ -1,8 +0,0 @@
-const loggingMiddleware = (store) => (next) => (action) => {
-  // Our middleware
-  console.log(`Redux Log:`, action)
-  // call the next function
-  next(action);
-}
-
-export default loggingMiddleware
--- day-22/src/redux/types.js
+++
@@ -1,4 +0,0 @@
-export const FETCH_NEW_TIME = 'FETCH_NEW_TIME';
-
-export const LOGIN = 'USER_LOGIN';
-export const LOGOUT = 'USER_LOGOUT';
